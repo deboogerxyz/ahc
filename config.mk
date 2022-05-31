4ddf82c4bc2b68e1fe98ec3cf60c49a19cccd912
debooger <debooger@debooger.xyz>
2022-05-31 20:11:40 +0200

    Enabled -fPIC compile option to fix symbol relocation

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200112L -D_XOPEN_SOURCE=500
-#CFLAGS   = -g -m32 -std=c89 -pedantic -Wall -Wno-deprecated-declarations -O0 ${CPPFLAGS}
-CFLAGS   = -m32 -std=c89 -pedantic -Wall -Wno-deprecated-declarations -Os ${CPPFLAGS}
+#CFLAGS   = -g -m32 -fPIC -std=c89 -pedantic -Wall -Wno-deprecated-declarations -O0 ${CPPFLAGS}
+CFLAGS   = -m32 -std=c89 -fPIC -pedantic -Wall -Wno-deprecated-declarations -Os ${CPPFLAGS}
 LDFLAGS  = -m32 -shared
 
 # compiler and linker
